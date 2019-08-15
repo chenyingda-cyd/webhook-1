a8cb0c23e0290c0685a24e4a79c94536700a57ab
chenyingdaxiaohao <cyd@rainchapter.com>
2019-08-15 16:59:21 +0800

    分支修改

--- app/service/test.js
+++ app/service/test.js
@@ -3,8 +3,8 @@
 module.exports = app => { 
   return class webhookService extends app.Service { 
     async index() { 
-      console.log('???')
-      console.log('from branch prod')
+      // console.log('???')
+      // console.log('from branch prod')
     }
   }
 }
